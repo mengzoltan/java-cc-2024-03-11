6d2eb9702041d07be9795be860a8385d11633617
Meng Zoltan <meng.zoltan@gmail.com>
2024-03-10 07:27:42 +0100

    mAdd unit tests

--- src/test/java/org/training/gildedrose/GildedRoseTest.java
+++ src/test/java/org/training/gildedrose/GildedRoseTest.java
@@ -5,7 +5,7 @@ package org.training.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
@@ -14,7 +14,134 @@ class GildedRoseTest {
         Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void sulfuras_quality_check() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 0, 80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, items[0].quality);
+        assertEquals(0, items[0].sellIn);
+        app.updateQuality();
+        assertEquals(80, items[0].quality);
+        assertEquals(0, items[0].sellIn);
+    }
+
+    @Test
+    void The_Quality_of_an_item_is_never_more_than_50() {
+        int quality = 48;
+        int sellIn = 0;
+        Item agedBrie = new Item("Aged Brie", sellIn, quality);
+        Item[] items = new Item[]{agedBrie};
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(quality+2, agedBrie.quality);
+        assertEquals(sellIn-1, agedBrie.sellIn);
+
+        app.updateQuality();
+
+        assertEquals(50, agedBrie.quality);
+        assertEquals(sellIn-2, agedBrie.sellIn);
+    }
+
+    @Test
+    void testQualityAndSellInDropOnSimpleItems() {
+        int quality = 2;
+        int sellIn = 4;
+        Item foo = new Item("foo", sellIn, quality);
+        Item[] items = new Item[]{foo};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals("foo", foo.name);
+        assertEquals(quality-1, foo.quality);
+        assertEquals(sellIn-1, foo.sellIn);
+    }
+
+    @Test
+    void The_Quality_of_an_item_is_never_negative() {
+        int quality = 0;
+        int sellIn = 0;
+        Item foo = new Item("foo", sellIn, quality);
+        Item[] items = new Item[]{foo};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals("foo", foo.name);
+        assertEquals(0, foo.quality);
+        assertEquals(sellIn-1, foo.sellIn);
+
+        app.updateQuality();
+        assertEquals(0, foo.quality);
+
+    }
+
+    @Test
+    void Once_the_sell_by_date_has_passed_Quality_degrades_twice_as_fast() {
+        int quality = 10;
+        int sellIn = 0;
+        Item foo = new Item("foo", sellIn, quality);
+        Item[] items = new Item[]{foo};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals("foo", foo.name);
+        assertEquals(quality-2, foo.quality);
+        assertEquals(sellIn-1, foo.sellIn);
+
+        app.updateQuality();
+
+        assertEquals("foo", foo.name);
+        assertEquals(quality-4, foo.quality);
+        assertEquals(sellIn-2, foo.sellIn);
+    }
+
+    @Test
+    void Aged_Brie_actually_increases_in_Quality_the_older_it_gets() {
+        int quality = 10;
+        int sellIn = 4;
+        Item agedBrie = new Item("Aged Brie", sellIn, quality);
+        Item[] items = new Item[]{agedBrie};
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(quality+1, agedBrie.quality);
+        assertEquals(sellIn-1, agedBrie.sellIn);
+
+        app.updateQuality();
+
+        assertEquals(quality+2, agedBrie.quality);
+        assertEquals(sellIn-2, agedBrie.sellIn);
+    }
+
+    @Test
+    void Aged_Brie_actually_increases_in_Quality_the_older_it_gets_twice_after_zero_sell_in() {
+        int quality = 10;
+        int sellIn = 0;
+        Item agedBrie = new Item("Aged Brie", sellIn, quality);
+        Item[] items = new Item[]{agedBrie};
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(quality+2, agedBrie.quality);
+        assertEquals(sellIn-1, agedBrie.sellIn);
+
+        app.updateQuality();
+
+        assertEquals(quality+4, agedBrie.quality);
+        assertEquals(sellIn-2, agedBrie.sellIn);
     }
 
 }
